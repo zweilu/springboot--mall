b29228c85ffe3735ad3b675ed6b9fb1a151cb770
wei <backlu84@gmail.com>
2023-12-24 20:05:36 +0800

    商品功能-實作查詢商品列表(排序)

--- SpringbootMall/src/main/java/com/example/wei/dao/impl/ProductDaoImpl.java
+++ SpringbootMall/src/main/java/com/example/wei/dao/impl/ProductDaoImpl.java
@@ -38,6 +38,8 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%"+productQueryParams.getSearch()+"%");
         }
 
+        sql = sql + " ORDER By " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
         return productList;
--- SpringbootMall/src/main/java/com/example/wei/dto/ProductQueryParams.java
+++ SpringbootMall/src/main/java/com/example/wei/dto/ProductQueryParams.java
@@ -7,6 +7,8 @@ import com.example.wei.constant.ProductCategory;
 public class ProductQueryParams {
     private ProductCategory category;
     private String search;
+    private String orderBy;
+    private String sort;
 
 
     public ProductCategory getCategory() {
@@ -23,5 +25,22 @@ public class ProductQueryParams {
 
     public void setSearch(String search) {
         this.search = search;
+    }
+
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
     }
 }
